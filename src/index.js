d738d44ffbd09fdebe01ad3dbfc1e16c343304b5
Lucas Abroms <lucasabroms@lucass-mbp.home>
2020-09-29 18:34:58 -0700

    asdf

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import express from 'express';
 
 import App from './App';
 
 const PORT = process.env.PORT || '8080'
 
-app = express();
+var app = express();
 
 app.set('port', PORT)
 
