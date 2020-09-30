a39283d8d42cd7bdf0fc6344f9a7d09f4e8007b2
Lucas Abroms <lucasabroms@lucass-mbp.home>
2020-09-29 19:55:57 -0700

    Updates

---
+++ precache-manifest.bfe4003a42072a4a1f155afdf21b1994.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1c72c7f605a2d5376b4b698dcbe16dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43355000353f5436ebc2",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7856f880e34d32e44643",
+    "url": "/static/css/main.d56f4533.chunk.css"
+  },
+  {
+    "revision": "43355000353f5436ebc2",
+    "url": "/static/js/2.dd493545.chunk.js"
+  },
+  {
+    "revision": "95bb60cd5d6c9a5adb06ecf57a72f072",
+    "url": "/static/js/2.dd493545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7856f880e34d32e44643",
+    "url": "/static/js/main.16513314.chunk.js"
+  },
+  {
+    "revision": "e15db08ebd4ba76f0a30",
+    "url": "/static/js/runtime-main.16eabf1c.js"
+  }
+]);
