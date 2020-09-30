810649335a307a072d936b4e3ab183ece2572b86
Lucas Abroms <lucasabroms@lucass-mbp.home>
2020-09-29 18:59:44 -0700

    changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import express from 'express';
 
 import App from './App';
 
